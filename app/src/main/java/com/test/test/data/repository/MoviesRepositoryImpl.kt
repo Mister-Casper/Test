4464bfb827f55e3b6bd7040d1cd130539e227a27
Noah <44705397+Mister-Casper@users.noreply.github.com>
2022-05-24 09:49:48 +0700

    оптимизация

--- app/src/main/java/com/test/test/data/repository/MoviesRepositoryImpl.kt
+++ app/src/main/java/com/test/test/data/repository/MoviesRepositoryImpl.kt
@@ -15,6 +15,10 @@ import com.test.test.domain.model.CharacterListing
 import com.test.test.domain.model.MovieListing
 import com.test.test.domain.repository.MoviesRepository
 import com.test.test.util.Resource
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import retrofit2.HttpException
@@ -28,7 +32,7 @@ class MoviesRepositoryImpl @Inject constructor(
     db: MoviesDatabase,
     private val jsonConverter: JsonConverter,
     private val releaseDateConverter: ReleaseDateConverter,
-    private val context: Context
+    private val context:Context
 ) : MoviesRepository {
 
     private val moviesDao = db.moviesDao
@@ -40,13 +44,7 @@ class MoviesRepositoryImpl @Inject constructor(
         return flow {
             emit(Resource.Loading(true))
             val localMovieListings = moviesDao.searchMovieListing(query)
-            emit(Resource.Success(data = localMovieListings.map {
-                it.toMovieListing(
-                    releaseDateConverter
-                )
-            }))
-            // Проверяем что локальная база пуста.
-            // localMovieListings будет пустым если попытаться найти фильм, но ничего не обнаружить, поэтому необходимо проверить оба условия.
+
             if (localMovieListings.isEmpty() && query.isBlank()) {
                 val remoteMovieListings = try {
                     jsonConverter.convertMoviesListings(moviesApi.getMovies().string())
@@ -63,12 +61,17 @@ class MoviesRepositoryImpl @Inject constructor(
                 remoteMovieListings?.let {
                     moviesDao.insertMovieListings(remoteMovieListings.map { it.toMovieListingEntity() })
                     emit(
-                        // Вернуть загруженные в кэш данные
                         Resource.Success(
                             moviesDao.searchMovieListing("")
                                 .map { it.toMovieListing(releaseDateConverter) })
                     )
                 }
+            }else if(localMovieListings.isNotEmpty()){
+                emit(Resource.Success(data = localMovieListings.map {
+                    it.toMovieListing(
+                        releaseDateConverter
+                    )
+                }))
             }
             emit(Resource.Loading(false))
         }
@@ -77,37 +80,41 @@ class MoviesRepositoryImpl @Inject constructor(
     override suspend fun getCharacters(episodeId: Int): Flow<Resource<List<CharacterListing>>> {
         return flow {
             emit(Resource.Loading(true))
-            val characters = mutableListOf<CharacterListingEntry>()
+            val characters = mutableListOf<Deferred<CharacterListingEntry>>()
             val characterIds =
                 moviesDao.searchMovieListing("").find { it.episode == episodeId }!!.charactersId
-            characterIds.forEach { id ->
-                val character = characterDao.getCharacterById(id)
-                if (character == null) {
-                    try {
-                        characters.add(
-                            jsonConverter.convertToCharacterListing(
-                                moviesApi.getCharacter(
-                                    id
-                                ).string()
-                            ).toCharacterLingEntity()
-                        )
-                    } catch (e: IOException) {
-                        e.printStackTrace()
-                        emit(Resource.Loading(false))
-                        emit(Resource.Error(context.getString(R.string.cant_load_data)))
-                        return@flow
-                    } catch (e: HttpException) {
-                        e.printStackTrace()
-                        emit(Resource.Loading(false))
-                        emit(Resource.Error(context.getString(R.string.cant_load_data)))
-                        return@flow
-                    }
-                }else
-                    characters.add(characterDao.getCharacterById(id)!!)
+            coroutineScope {
+                characterIds.forEach { id ->
+                    val character = characterDao.getCharacterById(id)
+                    if (character == null) {
+                        try {
+                            characters.add(
+                                async {
+                                    jsonConverter.convertToCharacterListing(
+                                        moviesApi.getCharacter(
+                                            id
+                                        ).string()
+                                    ).toCharacterLingEntity()
+                                }
+                            )
+                        } catch (e: IOException) {
+                            e.printStackTrace()
+                            emit(Resource.Loading(false))
+                            emit(Resource.Error(context.getString(R.string.cant_load_data)))
+                            return@coroutineScope
+                        } catch (e: HttpException) {
+                            e.printStackTrace()
+                            emit(Resource.Loading(false))
+                            emit(Resource.Error(context.getString(R.string.cant_load_data)))
+                            return@coroutineScope
+                        }
+                    } else
+                        characters.add(async{characterDao.getCharacterById(id)!!})
+                }
             }
-            characterDao.insertCharacterListings(characters)
+            characterDao.insertCharacterListings(characters.awaitAll())
             emit(Resource.Loading(false))
-            emit(Resource.Success(characters.map { it.toCharacterListing() }))
+            emit(Resource.Success(characters.awaitAll().map { it.toCharacterListing() }))
         }
     }
 
